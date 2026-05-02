b9a26277db28aaaf1e3c3d6f8b57d6adcea5a8cc
katapon-0 <ponomarevakata104@gmail.com>
2026-05-02 05:16:14 +0300

    для gh-pages

--- src/route.js
+++ src/route.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 
 import HomePage from "./components/pages/HomePage.vue"
 import LoginPage from "./components/auth/LoginPage.vue"
@@ -53,7 +53,7 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
  
